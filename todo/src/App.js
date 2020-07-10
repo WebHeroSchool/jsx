864677536e27c70ad17d8087eb8a83d535a15bc6
Artem Mezentsev <artemamway88@gmail.com>
2020-07-10 17:37:39 +0700

    Изменения в App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const num = 35;
+const flag = true;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {flag && 'Flag is true'}
         <img src={logo} className="App-logo" alt="logo" />
+        {48}
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {flag ? 'flag is true' : 'flag is false'}
         </p>
-        <p>
+        {null}
+        {false}
+        {undefined}
+        {true}
+        {num + 39}
+        <p style = {{ color: 'green' }}>
           Hello World!
-        </p> 
+        </p>
+        {num}
       </header>
     </div>
   );
